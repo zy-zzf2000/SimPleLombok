4ba864cad4167c4d9d631877ac7b1a4f4e13a5be
zy <953725892@qq.com>
2023-04-04 21:42:38 +0800

    test(People.java,complie.bat):完成getter注解的测试

---
+++ src/main/java/complie.bat
@@ -0,0 +1,2 @@
+javac -encoding UTF-8 .\org\example\processor\GenGetterProcessor.java
+javac -processor org.example.processor.GenGetterProcessor .\org\example\examples\People.java
---
+++ src/main/java/org/example/examples/People.java
@@ -0,0 +1,16 @@
+package org.example.examples;
+
+import org.example.anno.GenGetter;
+
+/**
+ * @author zy
+ * @version 1.0
+ * @description TODO
+ * @date 2023/4/4 16:15
+ */
+
+@GenGetter
+public class People {
+    private String name;
+    private int age;
+}
